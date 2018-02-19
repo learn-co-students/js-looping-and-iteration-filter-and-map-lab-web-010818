9c5504c2a15ba6b6cb517c9ebc2915cf82fb44b9
TorreJohnson <torre.johnson@gmail.com>
2018-02-19 16:55:33 -0500

    completed Filter and Map Lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution here:
+function driversWithRevenueOver(drivers, rev) {
+  return drivers.filter(function(user) {return user.revenue > rev})
+}
+
+function driverNamesWithRevenueOver(drivers, rev) {
+  const newArr = []
+  const filteredDrivers = driversWithRevenueOver(drivers, rev);
+  filteredDrivers.map(function(user) {newArr.push(user.name)})
+  return newArr;
+}
+
+function exactMatch(drivers, obj) {
+  return drivers.filter(function(user) {
+    for(const key in obj) {
+      if (user[key] === obj[key]) {
+        return user;
+      }
+    }
+  });
+}
+
+function exactMatchToList(drivers, obj) {
+  newArr = [];
+  exactMatch(drivers, obj).map(function(user) { newArr.push(user.name) });
+  return newArr;
+}
